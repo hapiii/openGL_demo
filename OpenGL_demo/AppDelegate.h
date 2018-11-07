3ec13e419e1091c2d7b211632f045b0d05055e22
WQ <869932084@qq.com>
2018-11-07 17:50:22 +0800

    Initial Commit

---
+++ OpenGL_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGL_demo
+//
+//  Created by 王强 on 2018/11/7.
+//  Copyright © 2018 鹰云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGL_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL_demo
+//
+//  Created by 王强 on 2018/11/7.
+//  Copyright © 2018 鹰云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
