4081665960609464eac4160a3006e83e830c6c3b
WQ <869932084@qq.com>
2018-11-08 15:04:33 +0800

    first

---
+++ OpenGL_demo/Controllers/BaseController.h
@@ -0,0 +1,17 @@
+//
+//  BaseController.h
+//  OpenGL_demo
+//
+//  Created by 王强 on 2018/11/7.
+//  Copyright © 2018 鹰云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaseController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OpenGL_demo/Controllers/OneController.m
@@ -0,0 +1,32 @@
+//
+//  OneController.m
+//  OpenGL_demo
+//
+//  Created by 王强 on 2018/11/7.
+//  Copyright © 2018 鹰云. All rights reserved.
+//
+
+#import "OneController.h"
+
+@interface OneController ()
+
+@end
+
+@implementation OneController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
